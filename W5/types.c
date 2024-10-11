1be5b8b09cb0ffc025ca080e966b5172a6f52c44
abbylxt <z5362344@unsw.edu.au>
2024-10-11 13:11:00 +1100

    w5 upload

---
+++ W5/types.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(void) {
+    // // dec counting: 0 1 2 ... 9
+    // int dec = 1234; // = 1234
+    // // hex counting: 0 1 2 ... 9 A B C D E F
+    // int hex = 0x1234; // = 4096 + 512 + 48 + 4 = 4,660
+    // // oct counting: 0 1 2 ... 7
+    // int oct = 01234; // = 668
+    // // binary = 0 1
+    // int binary = 0b1010; // 10 (not recommend to use in your code)
+
+    // 127
+    int dec = 1234; 
+    // hex counting: 0 1 2 ... 9 A B C D E F
+    int hex = 0x1234; //    0x4 = 4 * 16^0 =  4
+                      //                      +
+                      //   0x30 = 3 * 16^1 =  48
+                      //                      +
+                      //  0x200 = 2 * 16^2 =  512
+                      //                      +
+                      // 0x1000 = 1 * 16^3 =  4096
+                      //                      =
+                      //                      4660
+    // oct counting: 0 1 2 ... 8
+    int oct = 01234; //
+                     // 00004  = 4 * (8 ^ 0) = 4
+                     //                         +
+                     // 00030  = 3 * (8 ^ 1) = 24
+                     //                         +
+                     // 00200  = 2 * (8 ^ 2) = 128
+                     //                         +
+                     // 01000  = 1 * (8 ^ 3) = 512
+                     //                         =
+                     //                        668
+    int binary = 0b1010; //
+                         // 0b1000 = 1 * 2^3 = 8
+                         // 0b0000 = 0 * 2^2 = 0
+                         // 0b0010 = 1 * 2^1 = 2
+                         // 0b0000 = 0 * 2^0 = 0
+                         // total  =  0b1010 = 10
+    //  hex to binary    0xC8  = 1100 1000
+                         // 0xC = 12 = 1100
+                         // 0x8 = 8 = 1000
+                         //
+
+
+    // printf("%d\n", dec);
+    // printf("0x%X = %d\n", hex, hex);
+    // printf("0%o = %d\n", oct, oct);
+    // printf("%d\n", binary);
+    // printf("0x%08X, 0%08o\n", hex, oct);
+    // printf("0x%03x, 0%o\n", hex, oct);
+
+    // mini program to print input in octal, hex and dec
+    int input;
+    scanf("%d", &input);
+    printf("input decimcal form = %d\n input hex form  = 0x%03X\n input octal form = 0%o\n", input, input, input);
+
+    return EXIT_SUCCESS;
+}
